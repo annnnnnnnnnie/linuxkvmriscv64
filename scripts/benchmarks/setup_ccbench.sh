0af5a84651449899e1554708b33f908adcd43553
tp1119 <tp1119@ic.ac.uk>
2023-05-13 16:04:28 +0100

    separate ccbench running script
    into host and guest versions

--- scripts/benchmarks/setup_ccbench.sh
+++ scripts/benchmarks/setup_ccbench.sh
@@ -3,4 +3,5 @@ echo "pwd="$PWD
 
 mkdir -p $PWD/overlay/root/mem_benches/ccbench
 cp $PWD/ccbench/strided/strided $PWD/overlay/root/mem_benches/ccbench/
-cp $PWD/ccbench/strided/run_strided.sh $PWD/overlay/root/mem_benches/ccbench/
+cp $PWD/ccbench/strided/run_strided_guest.sh $PWD/overlay/root/mem_benches/ccbench/
+cp $PWD/ccbench/strided/run_strided_host.sh $PWD/overlay/root/mem_benches/ccbench/
--- scripts/kvmtool/run_guest.sh
+++ scripts/kvmtool/run_guest.sh
@@ -2,6 +2,5 @@ echo "Inserting kvm.ko"
 insmod kvm.ko
 
 echo "Starting guest"
-./lkvm-static run -m 1024 -c1 --console serial -p "console=ttyS0 earlycon" \
-    -k ./Image \
-    --debug
+
+./lkvm-static run -m 1G -c1 --console serial -p "console=ttyS0 earlycon" -k ./Image
