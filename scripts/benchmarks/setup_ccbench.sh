6f532ae2c173a35ebe09a3389e9ce12218a9e91f
tp1119 <tp1119@ic.ac.uk>
2023-05-29 15:50:00 +0100

    includes ccbench/caches

--- scripts/benchmarks/setup_ccbench.sh
+++ scripts/benchmarks/setup_ccbench.sh
@@ -2,6 +2,11 @@ echo "setup ccbench"
 echo "pwd="$PWD
 
 mkdir -p $PWD/overlay/root/mem_benches/ccbench
-cp $PWD/ccbench/strided/strided $PWD/overlay/root/mem_benches/ccbench/
-cp $PWD/ccbench/strided/run_strided_guest.sh $PWD/overlay/root/mem_benches/ccbench/
-cp $PWD/ccbench/strided/run_strided_host.sh $PWD/overlay/root/mem_benches/ccbench/
+dest=$PWD/overlay/root/mem_benches/ccbench/
+
+cp $PWD/ccbench/strided/strided $dest
+cp $PWD/ccbench/strided/run_strided_guest.sh $dest
+cp $PWD/ccbench/strided/run_strided_host.sh $dest
+
+cp $PWD/ccbench/caches/caches $dest
+cp $PWD/ccbench/caches/run_caches_host.sh $dest
