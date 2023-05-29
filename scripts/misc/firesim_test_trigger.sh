e6250808a0474b49682b80e76243d88b2371cd67
tp1119 <tp1119@ic.ac.uk>
2023-05-29 15:49:46 +0100

    include firesim test trigger script

---
+++ scripts/misc/firesim_test_trigger.sh
@@ -0,0 +1,4 @@
+FIRESIM_START_TRIGGER=/host/usr/bin/firesim-start-trigger
+FIRESIM_END_TRIGGER=/host/usr/bin/firesim-end-trigger
+
+firesim-start-trigger && firesim-end-trigger
--- scripts/setup_overlay.sh
+++ scripts/setup_overlay.sh
@@ -9,3 +9,4 @@ cp $PWD/br_output/Image $PWD/overlay/root/
 cp $BR/.config ./overlay/root/br_config
 
 cp $PWD/scripts/kvmtool/run_guest.sh $PWD/overlay/root/
+cp $PWD/scripts/misc/firesim_test_trigger.sh $PWD/overlay/root/
