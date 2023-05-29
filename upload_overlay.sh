ff7b6ad09f811076b06fcdb569f20a027e4567a4
tp1119 <tp1119@ic.ac.uk>
2023-05-29 15:48:50 +0100

    include prebuilt lkvm-static in overlay

--- upload_overlay.sh
+++ upload_overlay.sh
@@ -1,7 +1,7 @@
 target=membenches.zip
 
 rm -f $target
-zip -r $target overlay/root/mem_benches 
+zip -r $target overlay/root/mem_benches overlay/root/lkvm-static overlay/root/run_guest.sh overlay/root/firesim_test_trigger.sh
 
 BASEDIR=$(dirname "$0")
 
