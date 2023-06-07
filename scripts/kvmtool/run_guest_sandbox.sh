2c59b85d187fcedb19dbe65f7f2253b68b06ca63
tp1119 <tp1119@ic.ac.uk>
2023-06-07 11:04:52 +0000

    run guest sandbox now first displays the benchmark script

--- scripts/kvmtool/run_guest_sandbox.sh
+++ scripts/kvmtool/run_guest_sandbox.sh
@@ -1,10 +1,10 @@
 echo "Inserting kvm.ko"
 insmod /root/kvm.ko
 
+cat $1
 echo "Starting sandbox"
-
-/root/lkvm-static sandbox -m 1G -c1 --console serial -p "console=ttyS0 earlycon" -k /root/Image -- sh $1
+/root/lkvm-static sandbox -m 1G -c1 --console serial -p "console=ttyS0 earlycon" -k /root/Image -- sh /host/root/$1
 poweroff
 
 # Example input for $1:
-# /host/root/mem_benches/ccbench/run_caches_guest.sh
+# mem_benches/ccbench/run_caches_guest.sh
