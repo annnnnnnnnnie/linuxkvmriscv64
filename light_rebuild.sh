cfb14debf39a4aa1973eb146eca9e60f9074023f
tp1119 <tp1119@ic.ac.uk>
2023-06-13 21:39:46 +0100

    script to rebuild with overlay only However probably won't rebuild overlay

---
+++ light_rebuild.sh
@@ -0,0 +1,30 @@
+BR=~/buildroot-2023.02
+export PATH_TO_RISCV_TOOLCHAIN=~/riscv
+
+export PATH=$PATH_TO_RISCV_TOOLCHAIN/bin:$PATH
+export ARCH=riscv
+export CROSS_COMPILE=riscv64-unknown-linux-gnu-
+
+echo "removing built overlay"
+rm -rf overlay/
+mkdir -p overlay/root/
+
+sh ./scripts/setup_overlay.sh
+sh ./scripts/setup_benchmarks.sh
+
+echo "rebuild"
+cd $BR
+# make -j4 linux-rebuild
+make -j4
+cd -
+
+# sh ./scripts/get_br_output.sh
+# cp ./br_output/kvm.ko ./overlay/root/
+
+# echo "rebuild with br artifact"
+# cd $BR
+# make -j4
+# cd -
+
+echo "light rebuild complete"
+# sh ./scripts/get_br_output.sh
